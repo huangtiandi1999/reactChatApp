5149b826e97a8a0d5f80895c945cc664902af0f6
万宇轩 <yuxuan1491433427@qq.con>
2019-10-24 22:48:15 +0800

    feat: 新增调试跨域文件夹

---
+++ src/views/Rank/index.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+import styles from './index.less';
+
+class Rank extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>B站rank</div>
+      </Fragment>
+    )
+  }
+}
+
+export default Rank;
